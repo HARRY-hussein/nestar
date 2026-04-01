50c5cd3bb5cca9bc4da697f9b220bdc4d8bb1477
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-04-02 07:14:24 +0900

    feat: MITASK ZK accomplished

--- train.ts
+++ train.ts
@@ -1,3 +1,4 @@
+console.log('RESULT:');
 /* =======================================================
    |                     TASK ZJ                         |
    =======================================================
@@ -11,14 +12,39 @@ MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
 Yuqoridagi misolda, array nested bo'lgan holdatda ham,
 bizning function ularning yig'indisini hisoblab qaytarmoqda. */
 
-function reduceNestedArray(arr: any[]): number {
-  return arr.reduce((sum: number, item: any): number => {
-    if (Array.isArray(item)) {
-      return sum + reduceNestedArray(item);
-    }
-    return sum + item;
-  }, 0);
+/* function reduceNestedArray(arr: any[]): number {
+	return arr.reduce((sum: number, item: any): number => {
+		if (Array.isArray(item)) {
+			return sum + reduceNestedArray(item);
+		}
+		return sum + item;
+	}, 0);
 }
 
-console.log("RESULT:")
-console.log(reduceNestedArray([1, [1, 2, [4]]])); // 8
+console.log(reduceNestedArray([1, [1, 2, [4]]])); */
+
+/* =======================================================
+   |                     TASK ZK                         |
+   =======================================================
+
+Shart:
+
+Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha 
+bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
+MASALAN: printNumbers() */
+
+function printNumbers() {
+	let count = 1;
+
+	const intervalId = setInterval(() => {
+		console.log(count);
+
+		count++;
+
+		if (count > 5) {
+			clearInterval(intervalId);
+		}
+	}, 1000);
+}
+
+printNumbers();
