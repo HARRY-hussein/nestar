5dc85d06abf1cc7628219ad908f9d08ba9923eef
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-04-04 17:29:15 +0900

    feat: create member schema model

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class MemberService {
+    constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
   
   public async signup(): Promise<string> {
     return 'signup executed!';
