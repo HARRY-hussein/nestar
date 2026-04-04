94e87f68057a3c5bdd07037a36bc7169b510b9aa
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-04-04 22:08:09 +0900

    fix: integrate global pipe validation

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 
@@ -8,7 +8,7 @@ import { Member } from '../../libs/dto/member/member';
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {} // DI
 	@Mutation(() => Member)
-	@UsePipes(ValidationPipe) // DTO
+	// @UsePipes(ValidationPipe) // DTO
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
 		try {
 			console.log('Mutation: signup');
@@ -20,7 +20,6 @@ export class MemberResolver {
 		}
 	}
 	@Mutation(() => Member)
-	@UsePipes(ValidationPipe) // DTO
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
 		try {
 			console.log('Mutation: login');
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'; // class NestJS ilovasini yaratish uchun ishlatiladi.
 import { AppModule } from './app.module'; // Barcha boshqa modullar yig'ilgan asosiy module
+import { ValidationPipe } from '@nestjs/common';
 
 // Global Integration
 async function bootstrap() { // define: baza ulanishi, port tinglashini kutish kerak
   const app = await NestFactory.create(AppModule); // NestJS ilovasini yaratadi. AppModuleni beramiz: "shu moduldan boshlab ilovani qur" Express + NestJS
+  app.useGlobalPipes(new ValidationPipe()); // Pipe Validation Global tarzda integratsiyasi
   await app.listen(process.env.PORT_API ?? 3000); // envdagi portda sorovlarni tinglasin, port belgilanmagan bolsa 3000.
 }
 bootstrap(); // call
