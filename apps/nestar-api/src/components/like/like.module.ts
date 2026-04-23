d3d89a6edb131253777de5dd5445df882f98b51e
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-04-23 11:05:19 +0900

    feat: create likeService model

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LikeService } from './like.service';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			// DBni alohida ulab oldik va Database Connection mantig'i bn Schemani bog'ladik
+			{
+				name: 'Like', // shu nom ostida yoziladi
+				schema: LikeSchema, // object -> model; shu nom bn export qilingan schema Model
+			},
+		]),
+		// AuthModule, // both => member moduleni qurishda yordam beradigan boshqa modulelarni chaqirib oldik
+		// ViewModule, //
+	],
+	providers: [LikeService], // LikeModulega xizmat qiluvchi asosiy mantiqlar
+	exports: [LikeService],
+})
 export class LikeModule {}
+// boshqa modulelarni mantig'i uchun xizmat qiladi
